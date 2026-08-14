0ac82271f5bc7ed83f369f5455b22ae03e585577
bkallaus <3630735+bkallaus@users.noreply.github.com>
2026-08-14 15:08:15 +0000

    feat: Word Counter tool

--- src/components/side-nav.tsx
+++ src/components/side-nav.tsx
@@ -18,6 +18,7 @@ const navItems = [
   { href: '#px-to-rem', label: 'Px to Rem' },
   { href: '#lorem-ipsum-generator', label: 'Lorem Ipsum Generator' },
   { href: '#url-parser', label: 'URL Parser' },
+  { href: '#word-counter', label: 'Word Counter' },
 ];
 
 const NavLinks = ({ onClick }: { onClick?: () => void }) => (
